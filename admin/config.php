2d61bfdf7ecf4f00e3ddd8e3a85cac5b2a631712
Akashkr4044 <37487915+Akashkr4044@users.noreply.github.com>
2020-10-17 23:25:49 +0530

    Add files via upload

---
+++ admin/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$siteurl="http://localhost/Training/Projects/admin/";
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database="adminpanel";
+
+
+$conn = new mysqli($hostname, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
